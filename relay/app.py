e270310f9b961c4b29306b326208b690275d0a37
Mugisha Moses <mossplix@gmail.com>
2013-06-10 16:51:55 +0300

    handle value error

--- relay/app.py
+++ relay/app.py
@@ -9,20 +9,23 @@ class App(AppBase):
         reg=re.compile(r'(\d+(,\d+)?)')
         text=message.text
         country_code="256"
-        amount,sender,balance=map(lambda x:x[0].replace(",",""),reg.findall(text))
-        if sender.startswith('0'):
-            number = '%s%s' % (country_code, sender[1:])
-        elif number.startswith('+'):
-            number = '%s' % sender[1:]
-        elif number[:len(country_code)] != country_code:
-            number = '%s%s' % (country_code, sender)
-
-        payment=MobilePayment.objects.create(sender=number,amount=amount)
         try:
-            client=MClient.objects.get(external_id=sender)
-            payment.client=client
-            payment.save()
-        except MClient.DoesNotExist:
+            amount,sender,balance=map(lambda x:x[0].replace(",",""),reg.findall(text))
+            if sender.startswith('0'):
+                number = '%s%s' % (country_code, sender[1:])
+            elif number.startswith('+'):
+                number = '%s' % sender[1:]
+            elif number[:len(country_code)] != country_code:
+                number = '%s%s' % (country_code, sender)
+
+            payment=MobilePayment.objects.create(sender=number,amount=amount)
+            try:
+                client=MClient.objects.get(external_id=sender)
+                payment.client=client
+                payment.save()
+            except MClient.DoesNotExist:
+                pass
+        except ValueError:
             pass
 
 
